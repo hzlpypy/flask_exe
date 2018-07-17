e398f9632c21172abd6391105edec539d991cad1
hzl <1761784585@qq.com>
2018-07-17 21:16:21 +0800

    第一次提交

---
+++ manage.py
@@ -0,0 +1,13 @@
+from flask_script import Manager
+from flask_migrate import MigrateCommand
+
+from Apps import create_app
+
+app = create_app()
+manage = Manager(app=app)
+manage.add_command('db',MigrateCommand)
+
+
+
+if __name__ == '__main__':
+    manage.run()
